81d1b49c0927095d052b6413a4acdc3fd6410381
Noor Mowafaq <33134693+NurMowafaq@users.noreply.github.com>
2018-05-12 11:01:10 +0300

    Add files via upload

---
+++ AboutActivity.java
@@ -0,0 +1,67 @@
+package com.example.hp.aboutapp;
+
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class AboutActivity extends AppCompatActivity {
+    WebView webView;
+    SwipeRefreshLayout swipe;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        swipe = (SwipeRefreshLayout)findViewById(R.id.swipe);
+        swipe.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                WebAction();
+            }
+        });
+
+        WebAction();
+
+
+
+    }
+
+
+    public void WebAction(){
+
+        webView = (WebView) findViewById(R.id.WebV);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.loadUrl("file:///android_asset/about_us.html");
+        swipe.setRefreshing(true);
+        webView.setWebViewClient(new WebViewClient(){
+
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+
+                webView.loadUrl("https://blog.google.com/");
+
+            }
+
+            public void onPageFinished(WebView view, String url) {
+                // do your stuff here
+                swipe.setRefreshing(false);
+            }
+
+        });
+
+    }
+
+
+    @Override
+    public void onBackPressed(){
+
+        if (webView.canGoBack()){
+            webView.goBack();
+        }else {
+            finish();
+        }
+    }
+}
